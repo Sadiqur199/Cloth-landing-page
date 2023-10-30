77667700cf28b5d06088d86f8ad14f609af81baf
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-10-31 01:40:43 +0600

    update body

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -1,39 +1,86 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
-
-
 
 const Contact = () => {
   useEffect(() => {
     Aos.init({ duration: 2000 });
   }, []);
   return (
-    <div id='contact' className='p-10' >
-       <div class="min-h-screen flex items-center justify-center">
+    <div id="contact" className="p-10">
+      <div class="min-h-screen flex items-center justify-center">
         <div class="bg-white p-8 rounded-lg shadow-md w-full">
-            <h1 class="text-3xl font-bold mb-5 ">Contact Us</h1>
-            <p data-aos="fade-right" className='text-justify mb-5'>A contact form is a web-based form that allows website visitors to  communicate with the owner or operator of a website. It serves as a means for users to send messages, inquiries, feedback, or requests without the need for direct email communication.</p>
-            <form>
-                <div class="mb-4 ">
-                    <label for="name" class="block text-gray-600 text-sm font-medium mb-2">Name</label>
-                    <input data-aos="fade-right" type="text" id="name" name="name" class="w-full px-4 py-2 border border-gray-300 rounded-lg  focus:outline-none focus:border-blue-300" placeholder="Your Name" required/>
-                </div>
+          <h1 class="text-3xl font-bold mb-5 ">Contact Us</h1>
+          <p data-aos="fade-right" className="text-justify mb-5">
+            A contact form is a web-based form that allows website visitors to
+            communicate with the owner or operator of a website. It serves as a
+            means for users to send messages, inquiries, feedback, or requests
+            without the need for direct email communication.
+          </p>
+          <form>
+            <div class="mb-4 ">
+              <label
+                for="name"
+                class="block text-gray-600 text-sm font-medium mb-2"
+              >
+                Name
+              </label>
+              <input
+                data-aos="fade-right"
+                type="text"
+                id="name"
+                name="name"
+                class="w-full px-4 py-2 border border-gray-300 rounded-lg  focus:outline-none focus:border-blue-300"
+                placeholder="Your Name"
+                required
+              />
+            </div>
 
-                <div class="mb-4">
-                    <label for="email" class="block text-gray-600 text-sm font-medium mb-2">Email</label>
-                    <input data-aos="fade-left" type="email" id="email" name="email" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-300" placeholder="youremail@example.com" required/>
-                </div>
+            <div class="mb-4">
+              <label
+                for="email"
+                class="block text-gray-600 text-sm font-medium mb-2"
+              >
+                Email
+              </label>
+              <input
+                data-aos="fade-left"
+                type="email"
+                id="email"
+                name="email"
+                class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-300"
+                placeholder="youremail@example.com"
+                required
+              />
+            </div>
 
-                <div class="mb-4">
-                    <label for="message" class="block text-gray-600 text-sm font-medium mb-2">Message</label>
-                    <textarea data-aos="fade-right" id="message" name="message" rows="4" class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-300" placeholder="Your Message" required></textarea>
-                </div>
+            <div class="mb-4">
+              <label
+                for="message"
+                class="block text-gray-600 text-sm font-medium mb-2"
+              >
+                Message
+              </label>
+              <textarea
+                data-aos="fade-right"
+                id="message"
+                name="message"
+                rows="4"
+                class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-300"
+                placeholder="Your Message"
+                required
+              ></textarea>
+            </div>
 
-                <button type="submit" class="bg-[#529B03] hover:bg-[#399FA6] text-white py-2 px-4 rounded-lg">Submit</button>
-            </form>
+            <button
+              type="submit"
+              class="bg-[#529B03] hover:bg-[#399FA6] text-white py-2 px-4 rounded-lg"
+            >
+              Submit
+            </button>
+          </form>
         </div>
-    </div>
+      </div>
     </div>
   );
 };
--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,69 +1,70 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FaFacebook, FaLinkedin, FaTwitterSquare } from "react-icons/fa";
-import Aos from 'aos'
-import 'aos/dist/aos.css'
-
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Footer = () => {
-  useEffect(()=>{
-    Aos.init({duration: 2000})
-  },[])
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
   return (
     <>
-    <div className="bg-black p-14">
-      <footer className="footer mt-5 p-10text-white">
-        <nav>
-          <header className="footer text-white text-2xl font-bold">
-            NITEX
-          </header>
-        </nav>
+      <div className="bg-black p-14">
+        <footer className="footer mt-5 p-10text-white">
+          <nav>
+            <header className="footer text-white text-2xl font-bold">
+              NITEX
+            </header>
+          </nav>
 
-        <nav data-aos='fade-right'>
-          <header className="footer text-white text-lg font-bold">
-            Contact
-          </header>
-          <a className="link link-hover text-white">support@nitex.info</a>
-          <p className="text-white">
-            153 Curtain Rd, London EC2A 3QE,
-            <br></br>
-            United Kingdom
-          </p>
-        </nav>
+          <nav data-aos="fade-right">
+            <header className="footer text-white text-lg font-bold">
+              Contact
+            </header>
+            <a className="link link-hover text-white">support@nitex.info</a>
+            <p className="text-white">
+              153 Curtain Rd, London EC2A 3QE,
+              <br></br>
+              United Kingdom
+            </p>
+          </nav>
 
-        <nav data-aos='fade-down'>
-          <header className="footer text-white text-lg font-bold">
-            Services
-          </header>
-          <a className="link link-hover text-white">Branding</a>
-          <a className="link link-hover text-white">Design</a>
-          <a className="link link-hover text-white">Marketing</a>
-          <a className="link link-hover text-white">Advertisement</a>
-        </nav>
-        <nav data-aos='fade-left'>
-          <header className="footer text-white text-lg font-bold ">
-            Social
-          </header>
-          <div className="grid grid-flow-col gap-4">
-            <Link className="text-white">
-              {" "}
-              <FaFacebook size={20} />{" "}
-            </Link>
-            <Link className="text-white">
-              {" "}
-              <FaLinkedin size={20} />{" "}
-            </Link>
-            <Link className="text-white">
-              {" "}
-              <FaTwitterSquare size={20} />{" "}
-            </Link>
-          </div>
-        </nav>
-      </footer>
-    </div>
-    <div className="bg-black">
-    <p className="text-center text-white">Copyright © 2023 - MD SADIQUR RAHMAN ❣</p>
-    </div>
+          <nav data-aos="fade-down">
+            <header className="footer text-white text-lg font-bold">
+              Services
+            </header>
+            <a className="link link-hover text-white">Branding</a>
+            <a className="link link-hover text-white">Design</a>
+            <a className="link link-hover text-white">Marketing</a>
+            <a className="link link-hover text-white">Advertisement</a>
+          </nav>
+          <nav data-aos="fade-left">
+            <header className="footer text-white text-lg font-bold ">
+              Social
+            </header>
+            <div className="grid grid-flow-col gap-4">
+              <Link className="text-white">
+                {" "}
+                <FaFacebook size={20} />{" "}
+              </Link>
+              <Link className="text-white">
+                {" "}
+                <FaLinkedin size={20} />{" "}
+              </Link>
+              <Link className="text-white">
+                {" "}
+                <FaTwitterSquare size={20} />{" "}
+              </Link>
+            </div>
+          </nav>
+        </footer>
+      </div>
+      <div className="bg-black">
+        <p className="text-center text-white">
+          Copyright © 2023 - MD SADIQUR RAHMAN ❣
+        </p>
+      </div>
     </>
   );
 };
